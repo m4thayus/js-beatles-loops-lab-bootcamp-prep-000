ce44b15e1a36c971faa3cb22a6ec1c903f3ae59f
Matt Williams <matt.seongjun@gmail.com>
2019-03-19 18:26:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function theBeatlesPlay(musicians, instruments) {
   var theBeatles = []
   if (musicians.length == instruments.length) {
    for (var c = 0; c < musicians.length; c++) {
-     
+     theBeatles.push(musicians[c] + ' plays ' + instruments[c]);
    } 
   }
+  else;
+  return theBeatles;
 }
