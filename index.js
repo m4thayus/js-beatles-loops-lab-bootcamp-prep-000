c637562e2b85b5c7e462cff3d88722725fd12121
Matt Williams <matt.seongjun@gmail.com>
2019-03-19 18:33:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,9 +24,4 @@ const facts = [
   "He hated the sound of his own voice"
 ];
 
-console.log(johnLennonFacts([
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-]));
+console.log(johnLennonFacts(facts));
