23d2f695dd9c72e4c07870e8335dde0c81da5c0d
Matt Williams <matt.seongjun@gmail.com>
2019-03-19 18:27:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians, instruments) {
-  var theBeatles = []
+  var theBeatles = [];
   if (musicians.length == instruments.length) {
    for (var c = 0; c < musicians.length; c++) {
      theBeatles.push(musicians[c] + ' plays ' + instruments[c]);
